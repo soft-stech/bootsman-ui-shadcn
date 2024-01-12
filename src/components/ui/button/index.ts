b0a4f1d732c0dd2e2165a16985b1b9b1c1ebb48d
Roman Truschev <truschev@gmail.com>
2024-01-12 18:15:45 +0300

    ghost styles + active state

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -11,9 +11,10 @@ export const buttonVariants = cva(
           'bg-primary text-primary-foreground hover:bg-primary-hover active:bg-primary-active',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input text-accent-foreground bg-accent-background hover:bg-accent/[0.08] hover:bg-accent/[0.16]',
+          'border border-input text-accent-foreground bg-accent-background hover:bg-accent/[0.08] active:bg-accent/[0.16]',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost:
+          'text-accent-foreground bg-accent-background hover:bg-accent/[0.08] active:bg-accent/[0.16]',
         link: 'text-primary underline-offset-4 hover:underline'
       },
       size: {
