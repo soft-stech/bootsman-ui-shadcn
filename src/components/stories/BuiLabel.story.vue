17f5d5106786fcaf2ffd248d58282d97bd1be23a
Andrey Karavashkin <akaravashkin@stsoft.ru>
2024-01-15 17:11:21 +0300

    BuiLabel component

---
+++ src/components/stories/BuiLabel.story.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import BuiLabel from '@/components/ui/label/BuiLabel.vue'
+import BuiSwitch from '@/components/ui/switch/BuiSwitch.vue'
+
+const variant = 'default'
+</script>
+
+<template>
+  <Story title="BuiLabel" autoPropsDisabled :layout="{ type: 'grid', width: '50%' }">
+    <Variant :key="`variant`" :title="`${variant}`">
+      <div class="flex items-center space-x-2">
+        <BuiSwitch id="my-test-id" />
+        <BuiLabel for="my-test-id">My label</BuiLabel>
+      </div>
+    </Variant>
+  </Story>
+</template>
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import '../src/assets/main.css'
 
 export { default as BuiButton } from './components/ui/button/BuiButton.vue'
 export { default as BuiSwitch } from './components/ui/switch/BuiSwitch.vue'
+export { default as BuiLabel } from './components/ui/label/BuiLabel.vue'
