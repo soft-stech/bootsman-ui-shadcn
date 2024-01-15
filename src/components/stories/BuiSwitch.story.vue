fbd0c84f4f8f320cf48861f50a6d00efb7cde685
Andrey Karavashkin <akaravashkin@stsoft.ru>
2024-01-15 16:11:10 +0300

    BuiSwitch component (BuiToggle)

---
+++ src/components/stories/BuiSwitch.story.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import BuiSwitch from '@/components/ui/switch/BuiSwitch.vue'
+
+const variant = 'default';
+
+</script>
+
+<template>
+  <Story title="BuiSwitch" autoPropsDisabled :layout="{ type: 'grid', width: '33%' }">
+    <Variant :key="`variant`" :title="`${variant}`">
+      <BuiSwitch></BuiSwitch>
+    </Variant>
+
+    <Variant
+      :key="`disabled-variant`"
+      :title="`Disabled`"
+    >
+      <BuiSwitch disabled></BuiSwitch>
+    </Variant>
+
+    <Variant
+      :key="`disabled-variant`"
+      :title="`Disabled Checked`"
+    >
+      <BuiSwitch disabled checked></BuiSwitch>
+    </Variant>
+  </Story>
+</template>
