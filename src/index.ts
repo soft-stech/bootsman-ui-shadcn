62154b4f32309b043a04edc8ff08c3dcea8a7399
Roman Truschev <truschev@gmail.com>
2024-01-16 17:50:16 +0300

    added missed exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import '../src/assets/main.css'
 
+export { default as BuiAccordion } from './components/ui/accordion/BuiAccordion.vue'
+export { default as BuiAlert } from './components/ui/alert/BuiAlert.vue'
 export { default as BuiButton } from './components/ui/button/BuiButton.vue'
 export { default as BuiSwitch } from './components/ui/switch/BuiSwitch.vue'
 export { default as BuiLabel } from './components/ui/label/BuiLabel.vue'
