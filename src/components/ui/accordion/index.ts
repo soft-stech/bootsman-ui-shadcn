e5c1172cc74f3c7f9c0cb0c0c075a9a2f4173bee
RT <truschev@gmail.com>
2024-01-16 09:49:53 +0300

    Ref dh 1017 (#3)
    * BuiAccordion component - default
    
    * BuiAccordion component

---
+++ src/components/ui/accordion/index.ts
@@ -0,0 +1,4 @@
+export { default as Accordion } from './BuiAccordion.vue'
+export { default as AccordionContent } from './BuiAccordionContent.vue'
+export { default as AccordionItem } from './BuiAccordionItem.vue'
+export { default as AccordionTrigger } from './BuiAccordionTrigger.vue'
