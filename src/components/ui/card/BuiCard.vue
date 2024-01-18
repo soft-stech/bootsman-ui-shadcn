5ff4fbb94a887993596a5820cd4c056c551a8be7
Roman Truschev <truschev@gmail.com>
2024-01-18 16:51:12 +0300

    BuiCard & BuiProgress components - minimal styling

--- src/components/ui/card/BuiCard.vue
+++ src/components/ui/card/BuiCard.vue
@@ -10,7 +10,14 @@ const props = defineProps({
 </script>
 
 <template>
-  <div :class="cn('rounded-lg border bg-card text-card-foreground shadow-sm', props.class)">
+  <div
+    :class="
+      cn(
+        'shadow-card-shadow rounded border border-border/[0.16] bg-card text-card-foreground',
+        props.class
+      )
+    "
+  >
     <slot />
   </div>
 </template>
--- src/components/ui/progress/BuiProgress.vue
+++ src/components/ui/progress/BuiProgress.vue
@@ -1,37 +1,20 @@
 <script setup lang="ts">
-import {
-  ProgressIndicator,
-  ProgressRoot,
-  type ProgressRootProps,
-} from 'radix-vue'
+import { ProgressIndicator, ProgressRoot, type ProgressRootProps } from 'radix-vue'
 import { cn } from '@/lib/utils'
 
-const props = withDefaults(
-  defineProps<ProgressRootProps & { class?: string }>(),
-  {
-    class: '',
-    modelValue: 0,
-  },
-)
+const props = withDefaults(defineProps<ProgressRootProps & { class?: string }>(), {
+  class: '',
+  modelValue: 0
+})
 </script>
 
 <template>
   <ProgressRoot
-    :class="
-      cn(
-        'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
-        props.class,
-      )
-    "
+    :class="cn('relative h-4 w-full overflow-hidden rounded-full bg-muted', props.class)"
     v-bind="props"
   >
     <ProgressIndicator
-      :class="
-        cn(
-          'h-full w-full flex-1 duration-300 bg-foreground transition-all',
-          props.class,
-        )
-      "
+      :class="cn('h-full w-full flex-1 bg-primary transition-all duration-300', props.class)"
       :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
     />
   </ProgressRoot>
