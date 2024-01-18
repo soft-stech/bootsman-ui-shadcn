298a32fe0b46bf4e9f657ff1ff863f5a18561224
Andrey K <karavashkinandrey@gmail.com>
2024-01-18 15:55:36 +0300

    BuiPagination component (#11)
    * BuiPagination component
    
    * BuiPagination add export
    
    ---------
    
    Co-authored-by: Andrey Karavashkin <akaravashkin@stsoft.ru>

---
+++ src/components/ui/pagination/BuiPaginationLast.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { PaginationLast, type PaginationLastProps, useForwardProps } from 'radix-vue'
+import { ChevronsRight } from 'lucide-vue-next'
+import { BuiButton } from '@/components/ui/button'
+
+const props = withDefaults(defineProps<PaginationLastProps>(), {
+  asChild: true
+})
+const forwarded = useForwardProps(props)
+</script>
+
+<template>
+  <PaginationLast v-bind="forwarded">
+    <BuiButton class="h-10 w-10 p-0" variant="outline">
+      <slot>
+        <ChevronsRight class="h-4 w-4" />
+      </slot>
+    </BuiButton>
+  </PaginationLast>
+</template>
---
+++ src/components/ui/pagination/index.ts
@@ -0,0 +1,10 @@
+export {
+  PaginationRoot as BuiPagination,
+  PaginationList as BuiPaginationList,
+  PaginationListItem as BuiPaginationListItem
+} from 'radix-vue'
+export { default as BuiPaginationFirst } from './BuiPaginationFirst.vue'
+export { default as BuiPaginationLast } from './BuiPaginationLast.vue'
+export { default as BuiPaginationNext } from './BuiPaginationNext.vue'
+export { default as BuiPaginationPrev } from './BuiPaginationPrev.vue'
+export { default as BuiPaginationEllipsis } from './BuiPaginationEllipsis.vue'
