8269e1f7908337e17dc7db2be3783570447c4ff7
Andrey Karavashkin <akaravashkin@stsoft.ru>
2024-01-18 15:14:41 +0300

    BuiDataTable component initial

---
+++ src/components/stories/BuiDataTable.story.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import type { ColumnDef } from '@tanstack/vue-table'
+import { BuiDataTable } from '@/components/ui/table'
+import { ref } from 'vue'
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'status',
+    header: 'Status'
+  },
+  {
+    accessorKey: 'email',
+    header: 'Email'
+  },
+  {
+    accessorKey: 'amount',
+    header: 'Amount'
+  }
+]
+
+const data = ref<Payment[]>([
+  {
+    id: '1',
+    amount: 100,
+    status: 'pending',
+    email: 'm1@example.com'
+  },
+  {
+    id: '2',
+    amount: 200,
+    status: 'pending',
+    email: 'm2@example.com'
+  },
+  {
+    id: '3',
+    amount: 300,
+    status: 'processing',
+    email: 'm2@example.com'
+  },
+  {
+    id: '4',
+    amount: 150,
+    status: 'success',
+    email: 'm3@example.com'
+  }
+])
+</script>
+
+<template>
+  <Story title="BuiDataTable" autoPropsDisabled :layout="{ type: 'grid', width: '95%' }">
+    <Variant :key="`variant`" title="Common">
+      <BuiDataTable :columns="columns" :data="data" />
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/ui/table/BuiDataTable.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts" generic="TData, TValue">
+import type { ColumnDef } from '@tanstack/vue-table'
+import { FlexRender, getCoreRowModel, useVueTable } from '@tanstack/vue-table'
+
+import {
+  BuiTable,
+  BuiTableBody,
+  BuiTableCell,
+  BuiTableEmpty,
+  BuiTableHead,
+  BuiTableHeader,
+  BuiTableRow
+} from './'
+
+const props = defineProps<{
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}>()
+
+const table = useVueTable({
+  get data() {
+    return props.data
+  },
+  get columns() {
+    return props.columns
+  },
+  getCoreRowModel: getCoreRowModel()
+})
+</script>
+
+<template>
+  <BuiTable>
+    <BuiTableHeader>
+      <BuiTableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+        <BuiTableHead v-for="header in headerGroup.headers" :key="header.id">
+          <FlexRender
+            v-if="!header.isPlaceholder"
+            :render="header.column.columnDef.header"
+            :props="header.getContext()"
+          />
+        </BuiTableHead>
+      </BuiTableRow>
+    </BuiTableHeader>
+    <BuiTableBody>
+      <template v-if="table.getRowModel().rows?.length">
+        <BuiTableRow
+          v-for="row in table.getRowModel().rows"
+          :key="row.id"
+          :data-state="row.getIsSelected() ? 'selected' : undefined"
+        >
+          <BuiTableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+            <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+          </BuiTableCell>
+        </BuiTableRow>
+      </template>
+      <template v-else>
+        <BuiTableEmpty :colspan="columns.length">No data</BuiTableEmpty>
+      </template>
+    </BuiTableBody>
+  </BuiTable>
+</template>
