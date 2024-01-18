360dbcb2dc3456721cef7cefe727ece84db005d2
Roman Truschev <truschev@gmail.com>
2024-01-18 14:29:13 +0300

    BuiSelect component

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -11,10 +11,9 @@ export const buttonVariants = cva(
           'bg-primary text-primary-foreground hover:bg-primary-hover active:bg-primary-active',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
-          'border border-input text-accent-foreground bg-accent-background hover:bg-accent/[0.08] active:bg-accent/[0.16]',
+          'border border-input text-accent-foreground hover:bg-accent/[0.08] active:bg-accent/[0.16]',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost:
-          'text-accent-foreground bg-accent-background hover:bg-accent/[0.08] active:bg-accent/[0.16]',
+        ghost: 'text-accent-foreground hover:bg-accent/[0.08] active:bg-accent/[0.16]',
         link: 'text-primary underline-offset-4 hover:underline',
         none: 'border-none bg-transparent'
       },
---
+++ src/components/ui/select/BuiSelectTrigger.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { SelectIcon, SelectTrigger, type SelectTriggerProps } from 'radix-vue'
+import { ChevronDown } from 'lucide-vue-next'
+import { cn } from '@/lib/utils'
+
+const props = withDefaults(
+  defineProps<SelectTriggerProps & { class?: string; invalid?: boolean }>(),
+  {
+    class: '',
+    invalid: false
+  }
+)
+</script>
+
+<template>
+  <SelectTrigger
+    v-bind="props"
+    :class="[
+      cn(
+        'flex h-10 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:min-w-0 [&>span]:truncate',
+        props.class
+      ),
+      props.invalid ? 'ring-2 !ring-destructive placeholder:!text-destructive' : ''
+    ]"
+  >
+    <slot />
+    <SelectIcon as-child>
+      <ChevronDown class="h-4 w-4 opacity-50" />
+    </SelectIcon>
+  </SelectTrigger>
+</template>
