7aff95487707e298ffdbc4019c524cd373ece48b
Сосидка Илья <isosidka@stsoft.ru>
2024-01-18 12:33:09 +0300

    chore: Updated entry file

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,11 @@
 import '../src/assets/main.css'
 
-import BuiAccordion from './components/ui/accordion/BuiAccordion.vue'
-import BuiAlert from './components/ui/alert/BuiAlert.vue'
-import BuiInput from '@/components/ui/input/BuiInput.vue'
-import {
-  BuiForm,
-  BuiFormField,
-  BuiFormItem,
-  BuiFormLabel,
-  BuiFormControl,
-  BuiFormMessage,
-  BuiFormDescription
-} from '@/components/ui/form/index'
-import BuiButton from './components/ui/button/BuiButton.vue'
-import BuiSwitch from './components/ui/switch/BuiSwitch.vue'
-import BuiLabel from './components/ui/label/BuiLabel.vue'
-import BuiBadge from './components/ui/badge/BuiBadge.vue'
-import {
-  BuiTable,
-  BuiTableBody,
-  BuiTableCaption,
-  BuiTableCell,
-  BuiTableEmpty,
-  BuiTableHead,
-  BuiTableHeader,
-  BuiTableRow,
-  BuiTableFooter
-} from './components/ui/table/index'
-
-export {
-  BuiAccordion,
-  BuiAlert,
-  BuiInput,
-  BuiForm,
-  BuiFormField,
-  BuiFormItem,
-  BuiFormLabel,
-  BuiFormControl,
-  BuiFormMessage,
-  BuiFormDescription,
-  BuiButton,
-  BuiSwitch,
-  BuiLabel,
-  BuiBadge,
-  BuiTable,
-  BuiTableBody,
-  BuiTableCaption,
-  BuiTableCell,
-  BuiTableEmpty,
-  BuiTableHead,
-  BuiTableHeader,
-  BuiTableRow,
-  BuiTableFooter
-}
+export * from './components/ui/accordion'
+export * from './components/ui/alert'
+export * from './components/ui/input'
+export * from './components/ui/form'
+export * from './components/ui/button'
+export * from './components/ui/switch'
+export * from './components/ui/label'
+export * from './components/ui/badge'
+export * from './components/ui/table'
