79d64bacede62d50dd994759245f48765fd28bb0
RT <truschev@gmail.com>
2024-01-19 17:29:49 +0300

    BuiDropdownMenu & BuiContextMenu (#15)
    * BuiDropdownMenu & BuiContextMenu default
    
    * BuiDropdownMenu & BuiContextMenu styles

---
+++ src/components/ui/dropdown-menu/BuiDropdownMenuSubTrigger.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { DropdownMenuSubTrigger, type DropdownMenuSubTriggerProps } from 'radix-vue'
+import { ChevronRight } from 'lucide-vue-next'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<DropdownMenuSubTriggerProps & { class?: string }>()
+</script>
+
+<template>
+  <DropdownMenuSubTrigger
+    v-bind="props"
+    :class="[
+      cn(
+        'focus:bg-accent/[0.08] data-[state=open]:bg-accent/[0.08] flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none',
+        props.class
+      )
+    ]"
+  >
+    <slot />
+    <ChevronRight class="ml-auto h-4 w-4" />
+  </DropdownMenuSubTrigger>
+</template>
