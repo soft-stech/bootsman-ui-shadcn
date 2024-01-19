9e6dd7cdbd6b4c9278ade338210a59beacb99f27
Roman Truschev <truschev@gmail.com>
2024-01-19 12:14:17 +0300

    BuiTabs default styling

---
+++ src/components/ui/tabs/index.ts
@@ -0,0 +1,4 @@
+export { default as BuiTabs } from './BuiTabs.vue'
+export { default as BuiTabsTrigger } from './BuiTabsTrigger.vue'
+export { default as BuiTabsList } from './BuiTabsList.vue'
+export { default as BuiTabsContent } from './BuiTabsContent.vue'
