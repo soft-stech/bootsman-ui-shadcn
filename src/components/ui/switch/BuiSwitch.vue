44b304d026df9cd3f190572ddb585e8d54cf5f31
Andrey Karavashkin <akaravashkin@stsoft.ru>
2024-01-22 18:37:23 +0300

    BuiRadioGroup component

--- src/components/ui/switch/BuiSwitch.vue
+++ src/components/ui/switch/BuiSwitch.vue
@@ -21,7 +21,7 @@ const forwarded = useForwardPropsEmits(props, emits)
       cn(
         ` peer inline-flex h-[20px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border transition-colors
         focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed
-        data-[state=checked]:border-border data-[state=unchecked]:border-gray-300 data-[state=checked]:bg-input data-[state=unchecked]:bg-background
+        data-[state=checked]:border-border data-[state=unchecked]:border-gray-300 data-[state=checked]:bg-primary data-[state=unchecked]:bg-background
         data-[state=checked]:hover:bg-accent-foreground data-[state=unchecked]:hover:bg-gray-50 disabled:data-[state=checked]:border-accent/30 disabled:data-[state=checked]:bg-accent/30
         disabled:data-[state=unchecked]:bg-slate-300/[.16] dark:data-[state=unchecked]:border-border dark:data-[state=unchecked]:bg-input/15
         dark:data-[state=unchecked]:hover:bg-accent/30 dark:data-[state=checked]:hover:enabled:border-primary-hover dark:data-[state=checked]:hover:enabled:bg-primary-hover dark:data-[state=unchecked]:disabled:border-foreground/30 dark:data-[state=unchecked]:disabled:bg-foreground/30`,
