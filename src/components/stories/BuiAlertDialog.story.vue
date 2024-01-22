a412333a1ac290656d8e1ca87c8ec4a7a62be607
Roman Truschev <truschev@gmail.com>
2024-01-22 14:17:11 +0300

    BuiAlertDialog & BuiAspectRatio components

---
+++ src/components/stories/BuiAlertDialog.story.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import {
+  BuiAlertDialog,
+  BuiAlertDialogAction,
+  BuiAlertDialogCancel,
+  BuiAlertDialogContent,
+  BuiAlertDialogDescription,
+  BuiAlertDialogFooter,
+  BuiAlertDialogHeader,
+  BuiAlertDialogTitle,
+  BuiAlertDialogTrigger
+} from '@/components/ui/alert-dialog'
+import { BuiButton } from '@/components/ui/button'
+</script>
+
+<template>
+  <Story title="BuiAlertDialog" autoPropsDisabled>
+    <Variant title="Default">
+      <div class="flex justify-center p-10">
+        <BuiAlertDialog>
+          <BuiAlertDialogTrigger as-child>
+            <BuiButton variant="outline"> Show Dialog </BuiButton>
+          </BuiAlertDialogTrigger>
+          <BuiAlertDialogContent>
+            <BuiAlertDialogHeader>
+              <BuiAlertDialogTitle>Are you absolutely sure?</BuiAlertDialogTitle>
+              <BuiAlertDialogDescription>
+                This action cannot be undone. This will permanently delete your account and remove
+                your data from our servers.
+              </BuiAlertDialogDescription>
+            </BuiAlertDialogHeader>
+            <BuiAlertDialogFooter>
+              <BuiAlertDialogCancel>Cancel</BuiAlertDialogCancel>
+              <BuiAlertDialogAction>Continue</BuiAlertDialogAction>
+            </BuiAlertDialogFooter>
+          </BuiAlertDialogContent>
+        </BuiAlertDialog>
+      </div>
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/ui/alert-dialog/BuiAlertDialogFooter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+
+const props = defineProps({
+  class: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'mt-3.5 flex flex-col space-y-2 sm:flex-row sm:justify-end sm:space-x-2 sm:space-y-0',
+        props.class
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
