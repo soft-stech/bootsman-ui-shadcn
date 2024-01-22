752d6b64b7d0866bd387b481ecde2d85b7a688c3
Andrey K <karavashkinandrey@gmail.com>
2024-01-22 18:57:07 +0300

    BuiRadioGroup component (#22)
    * BuiRadioGroup component
    
    * BuiRadioGroup component fix
    
    ---------
    
    Co-authored-by: Andrey Karavashkin <akaravashkin@stsoft.ru>

---
+++ src/components/stories/BuiRadioGroup.story.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { BuiRadioGroup, BuiRadioGroupItem, BuiLabel } from '@/index'
+</script>
+
+<template>
+  <Story title="BuiRadioGroup" autoPropsDisabled :layout="{ type: 'grid', width: '50%' }">
+    <Variant :key="`variant`" :title="`Radio Group`">
+      <BuiRadioGroup default-value="option-one">
+        <div class="flex items-center space-x-2">
+          <BuiRadioGroupItem id="option-one" value="option-one" />
+          <BuiLabel for="option-one">Option One</BuiLabel>
+        </div>
+        <div class="flex items-center space-x-2">
+          <BuiRadioGroupItem id="option-two" value="option-two" />
+          <BuiLabel for="option-two">Option Two</BuiLabel>
+        </div>
+        <div class="flex items-center space-x-2">
+          <BuiRadioGroupItem id="option-three" value="option-three" disabled />
+          <BuiLabel for="option-three" class="opacity-50">Option Three Disabled</BuiLabel>
+        </div>
+      </BuiRadioGroup>
+    </Variant>
+  </Story>
+</template>
