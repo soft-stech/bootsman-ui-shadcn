b249c773bb3bdd1f6c72c701a547e75e6fb758fc
Andrey Karavashkin <akaravashkin@stsoft.ru>
2024-01-22 15:12:40 +0300

    BuiTooltip, BuiPopover, BuiSeparator components + Date Picker

---
+++ src/components/stories/BuiTooltip.story.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {
+  BuiButton,
+  BuiTooltip,
+  BuiTooltipContent,
+  BuiTooltipProvider,
+  BuiTooltipTrigger
+} from '@/index'
+</script>
+
+<template>
+  <Story title="BuiTooltip" autoPropsDisabled>
+    <Variant :key="`variant-default`" :title="`Tooltip`">
+      <div class="flex justify-center pt-12">
+        <BuiTooltipProvider>
+          <BuiTooltip>
+            <BuiTooltipTrigger as-child>
+              <BuiButton variant="outline"> Hover </BuiButton>
+            </BuiTooltipTrigger>
+            <BuiTooltipContent>
+              <p>Hello there</p>
+            </BuiTooltipContent>
+          </BuiTooltip>
+        </BuiTooltipProvider>
+      </div>
+    </Variant>
+  </Story>
+</template>
