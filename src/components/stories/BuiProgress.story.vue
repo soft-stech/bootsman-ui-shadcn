d37f01d23fe2fee04bf9d1923a65be90b31dc5c9
Roman Truschev <truschev@gmail.com>
2024-01-24 13:50:17 +0300

    BuiProgress rework

--- src/components/stories/BuiProgress.story.vue
+++ src/components/stories/BuiProgress.story.vue
@@ -1,36 +1,57 @@
 <script setup lang="ts">
-import { BuiProgress } from '@/components/ui/progress'
+import { BuiProgress, BuiSlider } from '@/index'
 
-const progress = ref(13)
+const modelValue = ref([5])
 
-import { ref, watchEffect } from 'vue'
-
-watchEffect((cleanupFn) => {
-  const timer = setTimeout(() => (progress.value = 90), 1000)
-  cleanupFn(() => clearTimeout(timer))
-})
+import { ref } from 'vue'
 </script>
 
 <template>
-  <Story title="BuiProgress" autoPropsDisabled :layout="{ type: 'grid', width: '20%' }">
-    <Variant title="5%">
-      <BuiProgress :model-value="5" />
+  <Story title="BuiProgress" autoPropsDisabled :layout="{ type: 'grid', width: '33%' }">
+    <Variant title="With slider">
+      <div class="m-4">
+        <div class="mb-3">Value: {{ modelValue[0] }}</div>
+        <div class="flex flex-col gap-2">
+          <BuiProgress :model-value="modelValue[0]" class="max-w-[300px]" />
+          <BuiSlider v-model="modelValue" :max="100" :step="1" class="max-w-[300px]" />
+        </div>
+      </div>
     </Variant>
 
-    <Variant title="25%">
-      <BuiProgress :model-value="25" />
+    <Variant title="Progress">
+      <div class="m-4 flex flex-col gap-2">
+        <div class="m-4 flex flex-col gap-2">
+          <BuiProgress :model-value="0" variant="sm" />
+          <BuiProgress :model-value="1" variant="sm" />
+          <BuiProgress :model-value="50" variant="sm" />
+          <BuiProgress :model-value="99" variant="sm" />
+          <BuiProgress :model-value="100" variant="sm" />
+        </div>
+
+        <div class="m-4 flex flex-col gap-2">
+          <BuiProgress :model-value="0" />
+          <BuiProgress :model-value="1" />
+          <BuiProgress :model-value="50" />
+          <BuiProgress :model-value="99" />
+          <BuiProgress :model-value="100" />
+        </div>
+
+        <div class="m-4 flex flex-col gap-2">
+          <BuiProgress :model-value="0" variant="lg" />
+          <BuiProgress :model-value="1" variant="lg" />
+          <BuiProgress :model-value="50" variant="lg" />
+          <BuiProgress :model-value="99" variant="lg" />
+          <BuiProgress :model-value="100" variant="lg" />
+        </div>
+      </div>
     </Variant>
 
-    <Variant title="75%">
-      <BuiProgress :model-value="75" />
-    </Variant>
-
-    <Variant title="100%">
-      <BuiProgress :model-value="100" />
-    </Variant>
-
-    <Variant title="From 13% to 90%">
-      <BuiProgress v-model="progress" />
+    <Variant title="Colors">
+      <div class="m-4 flex flex-col gap-2">
+        <BuiProgress :model-value="50" class="max-w-[300px] bg-emerald-500 after:bg-emerald-500" />
+        <BuiProgress :model-value="50" class="max-w-[300px] bg-purple-500 after:bg-purple-500" />
+        <BuiProgress :model-value="50" class="max-w-[300px] bg-orange-500 after:bg-orange-500" />
+      </div>
     </Variant>
   </Story>
 </template>
--- src/components/ui/progress/BuiProgress.vue
+++ src/components/ui/progress/BuiProgress.vue
@@ -1,21 +1,47 @@
 <script setup lang="ts">
 import { ProgressIndicator, ProgressRoot, type ProgressRootProps } from 'radix-vue'
 import { cn } from '@/lib/utils'
+import type { HTMLAttributes } from 'vue'
 
-const props = withDefaults(defineProps<ProgressRootProps & { class?: string }>(), {
-  class: '',
-  modelValue: 0
-})
+import { progressVariants, capVariants } from '.'
+
+const props = withDefaults(
+  defineProps<
+    ProgressRootProps & {
+      class?: HTMLAttributes['class']
+      variant?: NonNullable<Parameters<typeof progressVariants>[0]>['variant']
+    }
+  >(),
+  {
+    class: '',
+    modelValue: 0
+  }
+)
 </script>
 
 <template>
   <ProgressRoot
-    :class="cn('relative h-4 w-full overflow-hidden rounded-full bg-muted', props.class)"
+    :class="
+      cn(
+        `relative w-full overflow-hidden bg-primary bg-opacity-20 after:absolute after:right-0 after:top-0 after:bg-primary after:bg-opacity-10`,
+        progressVariants({ variant }),
+        props.class
+      )
+    "
     v-bind="props"
   >
     <ProgressIndicator
-      :class="cn('h-full w-full flex-1 bg-primary transition-all duration-300', props.class)"
-      :style="`transform: translateX(-${100 - (props.modelValue ?? 0)}%);`"
+      :class="
+        cn(
+          'absolute box-content flex-1 bg-primary bg-opacity-60 transition-all duration-300 after:absolute after:right-0 after:top-0 after:bg-primary',
+          progressVariants({ variant }),
+
+          props.class
+        )
+      "
+      :style="`width: calc(calc(100% - ${capVariants[variant ?? 'default']}) * ${
+        (props.modelValue ?? 0) * 0.01
+      })`"
     />
   </ProgressRoot>
 </template>
