35f50fd6ca24c0d8daa079da37d9226770496c04
Roman Truschev <truschev@gmail.com>
2024-01-24 15:41:56 +0300

    fixing opacity according to doc see https://tailwindcss.com/docs/customizing-colors#using-css-variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,8 +29,8 @@ module.exports = {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: 'hsl(var(--primary) / var(--tw-bg-opacity))',
-          foreground: 'hsl(var(--primary-foreground))'
+          DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
+          foreground: 'hsl(var(--primary-foreground) / <alpha-value>)'
         },
         'primary-hover': {
           DEFAULT: 'hsl(var(--primary-hover))'
