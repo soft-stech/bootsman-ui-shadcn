b49593701f587a1bb0e3c47258c8622e0cbefb97
Roman Truschev <truschev@gmail.com>
2024-01-25 16:45:45 +0300

    BuiSelect left padding fix

--- src/components/ui/select/BuiSelectLabel.vue
+++ src/components/ui/select/BuiSelectLabel.vue
@@ -6,7 +6,7 @@ const props = defineProps<SelectLabelProps & { class?: string }>()
 </script>
 
 <template>
-  <SelectLabel :class="cn('py-1.5 pl-4 pr-2 text-sm font-semibold', props.class)">
+  <SelectLabel :class="cn('py-1.5 pl-8 pr-2 text-sm font-semibold', props.class)">
     <slot />
   </SelectLabel>
 </template>
