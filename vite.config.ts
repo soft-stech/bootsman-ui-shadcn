4f48d1652697fa62a04f2e29c1d041fd6fd107c4
Сосидка Илья <isosidka@stsoft.ru>
2024-02-08 11:42:09 +0300

    fix: Types build fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts({ rollupTypes: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
