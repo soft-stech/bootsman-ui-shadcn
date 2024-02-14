09896110f7020bd7111613587aae9048d1ec252f
Сосидка Илья <isosidka@stsoft.ru>
2024-02-14 16:25:50 +0300

    perf: Experiment with treeshaking

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,49 @@
 import { fileURLToPath, URL } from 'node:url'
-import { resolve } from 'node:path'
+import { extname, relative, resolve } from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import { glob } from 'glob'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [vue(), !env.DOCS && dts({ rollupTypes: true })],
+    plugins: [
+      vue(),
+      !env.DOCS && dts({ rollupTypes: true, include: ['src/components/ui', 'src/lib'] })
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
+
     build: {
+      copyPublicDir: false,
+      target: 'esnext',
       lib: {
         entry: resolve(__dirname, 'src/index.ts'),
-        name: 'BootsmanUiShadcn',
-        fileName: 'bootsman-ui-shadcn'
+        formats: ['es']
       },
       rollupOptions: {
+        input: Object.fromEntries(
+          glob
+            .sync([
+              'src/components/ui/**/*.{ts,vue}',
+              'src/lib/**/*.{ts}',
+              'src/tailwind-preset.ts'
+            ])
+            .map((file) => [
+              relative('src', file.slice(0, file.length - extname(file).length)),
+              fileURLToPath(new URL(file, import.meta.url))
+            ])
+        ),
         external: ['vue'],
         output: {
+          assetFileNames: 'assets/[name][extname]',
+          entryFileNames: '[name].js',
           globals: {
             vue: 'Vue'
           }
