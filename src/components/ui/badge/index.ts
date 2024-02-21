708f69b44768cf9b81c0315543c73d5c2cba892b
Roman Truschev <truschev@gmail.com>
2024-02-21 20:44:18 +0300

    fix: fixed BuiBadge border-radius

--- src/components/ui/badge/index.ts
+++ src/components/ui/badge/index.ts
@@ -3,7 +3,7 @@ import { cva } from 'class-variance-authority'
 export { default as BuiBadge } from './BuiBadge.vue'
 
 export const badgeVariants = cva(
-  'px-2 py-1 inline-flex items-center rounded font-normal transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-[13px] leading-5 gap-1',
+  'px-3 py-1 inline-flex items-center rounded-full font-normal transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-[13px] leading-5 gap-1',
   {
     variants: {
       variant: {
