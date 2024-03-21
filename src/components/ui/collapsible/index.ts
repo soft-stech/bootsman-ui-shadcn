a57e98955360b366a631aa19ab1785a60a720b3d
Сосидка Илья <isosidka@stsoft.ru>
2024-03-21 12:02:19 +0300

    feat: Added BuiCollapsible

---
+++ src/components/ui/collapsible/index.ts
@@ -0,0 +1,3 @@
+export { default as BuiCollapsible } from './BuiCollapsible.vue'
+export { default as BuiCollapsibleTrigger } from './BuiCollapsibleTrigger.vue'
+export { default as BuiCollapsibleContent } from './BuiCollapsibleContent.vue'
