2152b72da3de623a37e96118e87c877943aa553e
Roman Truschev <truschev@gmail.com>
2024-04-04 12:57:00 +0300

    feat: Added BuiHealth

---
+++ src/components/stories/BuiHealth.story.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { BuiHealth, BuiSlider } from '@/index'
+
+import { computed, ref } from 'vue'
+
+const h1 = ref([1])
+const h2 = ref([3])
+const h3 = ref([1])
+
+const health1 = computed(() => {
+  return [
+    { id: 'Running', class: 'bg-green-500 after:bg-green-500', count: h1.value[0] },
+    { id: 'Info', class: 'bg-yellow-500 after:bg-yellow-500', count: h2.value[0] },
+    { id: 'Error', class: 'bg-red-500 after:bg-red-500', count: h3.value[0] }
+  ]
+})
+</script>
+
+<template>
+  <Story title="BuiHealth" autoPropsDisabled :layout="{ type: 'grid', width: '33%' }">
+    <Variant title="BuiHealth">
+      <div class="m-4">
+        <div class="flex flex-col gap-2">
+          <BuiHealth :model-value="health1" class="max-w-[300px]" />
+
+          {{ h1 }}
+          <BuiSlider v-model="h1" :step="1" :max="50" class="max-w-[300px]" />
+
+          {{ h2 }}
+          <BuiSlider v-model="h2" :step="1" :max="50" class="max-w-[300px]" />
+
+          {{ h3 }}
+          <BuiSlider v-model="h3" :step="1" :max="50" class="max-w-[300px]" />
+        </div>
+      </div>
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/ui/health/BuiHealth.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+import { type PrimitiveProps } from 'radix-vue'
+import { computed, type HTMLAttributes } from 'vue'
+
+import { capHealthVariants, healthVariants } from '.'
+
+export interface HealthRootProps extends PrimitiveProps {
+  modelValue?: { id: string; class: HTMLAttributes['class']; count: number }[]
+}
+
+const props = withDefaults(
+  defineProps<
+    HealthRootProps & {
+      class?: HTMLAttributes['class']
+      variant?: NonNullable<Parameters<typeof healthVariants>[0]>['variant']
+    }
+  >(),
+  {
+    class: '',
+    modelValue: () => []
+  }
+)
+
+const total = computed(() => {
+  return props.modelValue.reduce((acc, item) => acc + item.count, 0)
+})
+
+const indicators = computed(() => {
+  let left = 0
+  return props.modelValue
+    .map((indicator) => {
+      const value = (indicator.count / total.value) * 100
+
+      const newIndicator = {
+        ...indicator,
+        left: left,
+        value
+      }
+
+      left += value
+
+      return newIndicator
+    })
+    .filter((indicator) => indicator.count > 0)
+})
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        `relative w-full overflow-hidden bg-primary bg-opacity-20 after:absolute after:right-0 after:top-0 after:bg-primary after:bg-opacity-10`,
+        healthVariants({ variant }),
+        props.class
+      )
+    "
+    v-bind="props"
+  >
+    <template v-for="indicator in indicators" :key="indicator.id">
+      <div
+        :class="
+          cn(
+            'absolute box-content flex-1 bg-primary transition-all duration-300 after:absolute after:right-0 after:top-0 after:bg-primary',
+            healthVariants({ variant }),
+            indicator.class
+          )
+        "
+        :style="`width: calc(calc(100% - ${capHealthVariants[variant ?? 'default']}) * ${
+          indicator.value * 0.01
+        }); left: calc(calc(100% - ${capHealthVariants[variant ?? 'default']}) * ${
+          indicator.left * 0.01
+        });`"
+      />
+    </template>
+  </div>
+</template>
