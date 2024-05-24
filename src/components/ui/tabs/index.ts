14dc19f276eb8e386855f2b70b9f8dcec1c0a022
Сосидка Илья <isosidka@stsoft.ru>
2024-05-24 19:42:02 +0300

    fix: Fixed card background on dark theme and refactored tab styles

--- src/components/ui/tabs/index.ts
+++ src/components/ui/tabs/index.ts
@@ -5,10 +5,10 @@ export { default as BuiTabsContent } from './BuiTabsContent.vue'
 
 import { cva } from 'class-variance-authority'
 
-export const tabsListVariants = cva('inline-flex h-10 items-center justify-center rounded-md p-1', {
+export const tabsListVariants = cva('inline-flex items-center justify-center rounded-md p-1', {
   variants: {
     variant: {
-      default: 'bg-muted text-foreground text-muted-foreground',
+      default: 'bg-muted text-muted-foreground p-0',
       ghost: 'bg-background bg-text-foreground'
     }
   },
@@ -18,11 +18,12 @@ export const tabsListVariants = cva('inline-flex h-10 items-center justify-cente
 })
 
 export const tabsTriggerVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap  px-3 py-1.5 text-md font-semibold ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50   data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+  'inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-md font-semibold ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground',
   {
     variants: {
       variant: {
-        default: 'rounded-sm data-[state=active]:bg-background',
+        default:
+          'rounded-sm data-[state=active]:bg-background data-[state=active]:border border-primary/[0.16] text-primary data-[state=active]:text-foreground data-[state=active]:shadow-tab-shadow',
         ghost:
           'border-transparent text-muted-foreground border-b-2 data-[state=active]:border-primary '
       }
