6c414a69b7afc1b28fa7f73f737af1a137dfc5fa
Roman Truschev <truschev@gmail.com>
2024-12-25 14:07:15 +0300

    fix: readonly input styles close to disabled

--- src/components/ui/input/index.ts
+++ src/components/ui/input/index.ts
@@ -5,7 +5,7 @@ export { default as BuiPasswordInput } from './BuiPasswordInput.vue'
 export { default as BuiBadgedInput } from './BuiBadgedInput.vue'
 
 export const inputVariants = cva(
-  'flex h-10 w-full rounded bg-background border px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30 dark:disabled:opacity-55 aria-[invalid=true]:border-destructive-foreground',
+  'flex h-10 w-full rounded bg-background border px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30 dark:disabled:opacity-55 read-only:opacity-30 dark:read-only:opacity-55 aria-[invalid=true]:border-destructive-foreground',
   {
     variants: {
       variant: {
