d8a468120e59b4a13131af32244f304519d46e06
Andrey Karavashkin <akaravashkin@stsoft.ru>
2025-01-21 13:20:18 +0300

    fix: Change color of BuiButton variant=outline

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -16,11 +16,11 @@ export const buttonVariants = cva(
         outline: [
           /* background*/ 'hover:bg-accent/[0.08] active:bg-accent/[0.16] disabled:bg-accent/[0.04]',
           /* border */ 'border border-primary disabled:border-accent/[0.32]',
-          /* text */ 'text-accent-foreground disabled:text-accent/[0.32]'
+          /* text */ 'text-accent disabled:text-accent/[0.32]'
         ],
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: [
-          /* text */ 'text-accent-foreground disabled:text-accent/[0.56]',
+          /* text */ 'text-accent disabled:text-accent/[0.56]',
           /* background */ 'hover:bg-accent/[0.08] active:bg-accent/[0.16]'
         ],
         link: 'text-primary underline-offset-4 hover:underline',
