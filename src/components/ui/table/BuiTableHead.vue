bc1f56ad0165fa08b31065421bc0fcf768676b74
Elena Vlasova <evlasova@stsoft.ru>
2025-02-10 12:36:50 +0300

    feat: Возможность стики заголовка и скролла таблицы (#51)
    * feat: Возможность стики заголовка и скролла таблицы
    * Удалена лишняя история
    * Фикс растяжения таблицы

--- src/components/ui/table/BuiTableHead.vue
+++ src/components/ui/table/BuiTableHead.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
 
-const props = defineProps<{ class?: string }>()
+const props = defineProps<{
+  class?: string
+  freezeHeader?: boolean
+}>()
 </script>
 
 <template>
   <th
     :class="
       cn(
-        'h-10 bg-foreground/[0.04] text-left align-middle text-foreground [&:has([role=checkbox])]:pr-0 ',
-        props.class
+        'h-10 text-left align-middle text-foreground [&:has([role=checkbox])]:pr-0 ',
+        props.class,
+        props.freezeHeader ? 'bg-background p-0' : 'bg-foreground/[0.04]'
       )
     "
   >
     <div
+      v-if="props.freezeHeader"
+      class="flex h-full items-center border-b border-border/[0.16] bg-foreground/[0.04]"
+    >
+      <div
+        class="flex h-8 w-full items-center whitespace-nowrap border-r border-border/[0.16] px-3 text-sm font-semibold"
+      >
+        <slot />
+      </div>
+    </div>
+    <div
+      v-else
       class="flex h-8 items-center whitespace-nowrap border-r border-border/[0.16] px-3 py-1 text-sm font-semibold"
     >
       <slot />
--- src/components/ui/table/BuiTableHeader.vue
+++ src/components/ui/table/BuiTableHeader.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
 
-const props = defineProps<{ class?: string }>()
+const props = defineProps<{
+  class?: string
+  freezeHeader?: boolean
+}>()
 </script>
 
 <template>
   <thead
     :class="
       cn(
-        '[&_th:first-child>div]:pl-6 [&_th:last-child>div]:border-0 [&_th:last-child>div]:pr-6 [&_tr]:border-b [&_tr]:border-border/[0.16]',
-        props.class
+        '[&_tr]:border-border/[0.16]',
+        props.class,
+        props.freezeHeader
+          ? 'sticky top-0 z-[1] [&_th:first-child>div>div]:pl-6 [&_th:last-child>div>div]:border-r-0 [&_th:last-child>div>div]:pr-6 [&_tr:last-child]:border-b-0'
+          : '[&_th:first-child>div]:pl-6 [&_th:last-child>div]:border-0 [&_th:last-child>div]:pr-6 [&_tr]:border-b'
       )
     "
   >
