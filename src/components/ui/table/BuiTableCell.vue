a7ae07eed93a2ba8361b5f1cd55b82c0ef05397e
evlasova <evlasova@stsoft.ru>
2025-07-08 14:11:33 +0300

    feat: Проба переноса слов в заголовках таблицы + меньше паддинги  (refs: #DH-2861)

--- src/components/ui/table/BuiTableCell.vue
+++ src/components/ui/table/BuiTableCell.vue
@@ -8,7 +8,7 @@ const props = defineProps<{ class?: string }>()
   <td
     :class="
       cn(
-        'h-12 break-words px-3 py-1 align-middle text-xs first:pl-6 last:pr-6 [&:has([role=checkbox])]:pr-0',
+        'h-12 break-words px-2 py-1 align-middle text-xs first:pl-3 last:pr-3 [&:has([role=checkbox])]:pr-0',
         props.class
       )
     "
--- src/components/ui/table/BuiTableHeader.vue
+++ src/components/ui/table/BuiTableHeader.vue
@@ -14,8 +14,8 @@ const props = defineProps<{
         '[&_tr]:border-border/[0.16]',
         props.class,
         props.freezeHeader
-          ? 'sticky top-0 z-[1] [&_th:first-child>div>div]:pl-6 [&_th:last-child>div>div]:border-r-0 [&_th:last-child>div>div]:pr-14 [&_tr:last-child]:border-b-0'
-          : '[&_th:first-child>div]:pl-6 [&_th:last-child>div]:border-0 [&_th:last-child>div]:pr-14 [&_tr]:border-b'
+          ? 'sticky top-0 z-[1] [&_th:first-child>div>div]:pl-3 [&_th:last-child>div>div]:border-r-0 [&_th:last-child>div>div]:pr-14 [&_tr:last-child]:border-b-0'
+          : '[&_th:first-child>div]:pl-3 [&_th:last-child>div]:border-0 [&_th:last-child>div]:pr-14 [&_tr]:border-b'
       )
     "
   >
