49d8b28a70cc65255431909e01c7dbdca054c1a8
Andrey Karavashkin <akaravashkin@stsoft.ru>
2025-10-03 17:50:41 +0300

    fix: Move some styles between projects

--- src/components/color-picker/BuiColorPicker.vue
+++ src/components/color-picker/BuiColorPicker.vue
@@ -156,11 +156,7 @@ watchThrottled(
 )
 </script>
 <template>
-  <Primitive
-    :as="as"
-    class="data-disabled:opacity-75"
-    :data-disabled="disabled ? true : undefined"
-  >
+  <Primitive :as="as" class="data-disabled:opacity-75" :data-disabled="disabled ? true : undefined">
     <div class="flex gap-4">
       <div ref="selectorRef" :class="colorPickerSelectorVariants({ size })" :style="selectorStyle">
         <div class="relative h-full w-full rounded-md" data-color-picker-background>
@@ -168,7 +164,7 @@ watchThrottled(
             ref="selectorThumbRef"
             :class="
               cn(
-                'absolute size-4 -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full ring-2 ring-background data-disabled:cursor-not-allowed',
+                'ring-background absolute size-4 -translate-x-1/2 -translate-y-1/2 cursor-pointer rounded-full ring-2 data-disabled:cursor-not-allowed',
                 ui?.selectorClass
               )
             "
@@ -182,7 +178,7 @@ watchThrottled(
           ref="trackThumbRef"
           :class="
             cn(
-              'absolute size-4 -translate-x-[4px] -translate-y-1/2 transform cursor-pointer rounded-full ring-2 ring-background data-disabled:cursor-not-allowed rtl:translate-x-[4px]',
+              'ring-background absolute size-4 -translate-x-[4px] -translate-y-1/2 transform cursor-pointer rounded-full ring-2 data-disabled:cursor-not-allowed rtl:translate-x-[4px]',
               ui?.trackClass
             )
           "
@@ -193,23 +189,3 @@ watchThrottled(
     </div>
   </Primitive>
 </template>
-
-<style scoped>
-[data-color-picker-background] {
-  background-image: linear-gradient(to top, #000 0%, rgba(0, 0, 0, 0) 100%),
-    linear-gradient(to right, #fff 0%, rgba(255, 255, 255, 0) 100%);
-}
-
-[data-color-picker-track] {
-  background-image: linear-gradient(
-    0deg,
-    red 0,
-    #f0f 17%,
-    #00f 33%,
-    #0ff 50%,
-    #0f0 67%,
-    #ff0 83%,
-    red
-  );
-}
-</style>
