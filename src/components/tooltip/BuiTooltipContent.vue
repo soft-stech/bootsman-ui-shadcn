f1e2d83be25c1f01f806e6b6315b2d474d0e9460
Svetlana Kochetygova <107425580+svetlanak-git@users.noreply.github.com>
2026-05-21 16:59:57 +0300

    feat: Варианты для тултипа (#refs: PBDEV-4836) (#92)

--- src/components/tooltip/BuiTooltipContent.vue
+++ src/components/tooltip/BuiTooltipContent.vue
@@ -7,8 +7,13 @@ import {
   useForwardPropsEmits
 } from 'reka-ui'
 import { cn } from '@/lib/utils'
+import { tooltipVariants } from '.'
 
-const props = withDefaults(defineProps<TooltipContentProps>(), {
+interface Props extends TooltipContentProps {
+  variant?: NonNullable<Parameters<typeof tooltipVariants>[0]>['variant']
+}
+
+const props = withDefaults(defineProps<Props>(), {
   sideOffset: 4
 })
 const emits = defineEmits<TooltipContentEmits>()
@@ -22,7 +27,8 @@ const forwarded = useForwardPropsEmits(props, emits)
       v-bind="{ ...forwarded, ...$attrs }"
       :class="
         cn(
-          'border-muted text-primary-foreground shadow-level3 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-tooltip [&_a]:text-tooltip-link z-50 overflow-hidden rounded-md border px-3 py-1.5 text-sm [&_a]:underline',
+          'shadow-level3 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 bg-tooltip [&_a]:text-tooltip-link z-50 overflow-hidden rounded-md border px-3 py-1.5 text-sm [&_a]:underline',
+          tooltipVariants({ variant }),
           $attrs.class ?? ''
         )
       "
--- src/components/tooltip/index.ts
+++ src/components/tooltip/index.ts
@@ -2,3 +2,22 @@ export { default as BuiTooltip } from './BuiTooltip.vue'
 export { default as BuiTooltipContent } from './BuiTooltipContent.vue'
 export { default as BuiTooltipTrigger } from './BuiTooltipTrigger.vue'
 export { default as BuiTooltipProvider } from './BuiTooltipProvider.vue'
+import { cva } from 'class-variance-authority'
+
+export const tooltipVariants = cva('', {
+  variants: {
+    variant: {
+      default: 'border-muted text-primary-foreground',
+      destructive:
+        'border-0 border-l-2 bg-destructive border-destructive-foreground text-destructive-foreground [&_svg]:text-destructive-foreground [&_div]:[scrollbar-color:initial]',
+      success:
+        'border-0 border-l-2 bg-success border-success-foreground text-success-foreground [&_svg]:text-success-foreground [&_div]:[scrollbar-color:initial]',
+      warning:
+        'border-0 border-l-2 bg-warning border-warning-foreground text-warning-foreground [&_svg]:text-warning-foreground [&_div]:[scrollbar-color:initial]',
+      info: 'border-0 border-l-2 bg-info border-info-foreground text-info-foreground [&_svg]:text-info-foreground [&_div]:[scrollbar-color:initial]'
+    }
+  },
+  defaultVariants: {
+    variant: 'default'
+  }
+})
