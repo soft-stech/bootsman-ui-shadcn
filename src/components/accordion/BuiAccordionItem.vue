80f590302280c593073711ce515ed8a38ceaef02
svetlanak-git <svetlanak.cf@gmail.com>
2026-06-02 17:17:14 +0300

    feat: Редизайн аккордеонов (refs: #PBDEV-4880)

--- src/components/accordion/BuiAccordionItem.vue
+++ src/components/accordion/BuiAccordionItem.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { AccordionItem, type AccordionItemProps } from 'reka-ui'
 import { cn } from '@/lib/utils'
+import { computed } from 'vue'
 
 const props = defineProps<AccordionItemProps & { class?: string }>()
+const attrs = computed(() => {
+  const { class: _, ...rest } = props
+  return rest
+})
 </script>
 
 <template>
-  <AccordionItem v-bind="props" :class="cn('rounded-sm border border-border ', props.class ?? '')">
+  <AccordionItem
+    v-bind="attrs"
+    :class="cn('border-border/16 shadow-level0 rounded-sm border', props.class ?? '')"
+  >
     <slot />
   </AccordionItem>
 </template>
