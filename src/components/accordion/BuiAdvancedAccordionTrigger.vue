d29cb8f611813dafbdde387978641e5c1e5163a6
Svetlana Kochetygova <107425580+svetlanak-git@users.noreply.github.com>
2026-06-11 13:15:34 +0300

    feat: Редизайн аккордеонов ч2 (refs: #PBDEV-4880) (#95)

--- src/components/accordion/BuiAdvancedAccordionTrigger.vue
+++ src/components/accordion/BuiAdvancedAccordionTrigger.vue
@@ -10,6 +10,7 @@ const props = defineProps<
     class?: string
     borderColorClass?: string
     bgColorClass?: string
+    hoverColorClass?: string
     iconBgClass?: string
     descriptionColorClass?: string
     isIconHidden?: boolean
@@ -22,50 +23,50 @@ const props = defineProps<
   <AccordionHeader
     :class="
       cn(
-        'flex flex-col p-2 data-[state=closed]:rounded-sm data-[state=open]:rounded-t-sm data-[state=open]:border-b',
-        props.borderColorClass ? props.borderColorClass : 'border-border',
-        props.bgColorClass ? props.bgColorClass : 'bg-primary/16'
+        'border-border/16 bg-foreground/4 flex min-h-12 items-center px-2 py-1 data-[state=closed]:rounded-sm data-[state=open]:rounded-t-sm data-[state=open]:border-b',
+        props.borderColorClass ?? '',
+        props.bgColorClass ?? '',
+        props.hoverColorClass ?? ''
       )
     "
     as="div"
   >
-    <div class="flex justify-between gap-2">
+    <div class="flex w-full items-center justify-between gap-2">
       <AccordionTrigger
         v-bind="props"
         :class="
           cn(
-            'ring-offset-background focus-visible:ring-ring flex flex-1 items-center gap-2 rounded-sm align-middle text-base leading-6 font-semibold transition-all hover:underline focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:pointer-events-none [&[data-state=open]>svg]:rotate-180',
+            'text-foreground ring-offset-background focus-visible:ring-ring relative flex flex-1 items-center gap-2 rounded-sm align-middle text-base leading-6 font-semibold transition-all focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-30 [&[data-state=open]>svg]:rotate-180',
             props.class,
-            props.isDisabledAsNormal ? 'disabled:opacity-100' : 'disabled:opacity-30'
+            props.isDisabledAsNormal ? 'disabled:opacity-100' : ''
           )
         "
       >
         <ChevronDown
           :class="
             cn(
-              'text-primary-foreground h-4 w-4 shrink-0 rounded-sm transition-transform duration-200',
-              props.iconBgClass
-                ? `${props.iconBgClass} dark:${props.iconBgClass}`
-                : 'bg-primary/[0.56] dark:bg-primary',
+              'bg-primary/[0.56] dark:bg-primary text-primary-foreground h-4 w-4 shrink-0 rounded-sm transition-transform duration-200',
+              props.iconBgClass ? `${props.iconBgClass} dark:${props.iconBgClass}` : '',
               props.isIconHidden ? 'invisible' : 'visible'
             )
           "
         />
-        <slot />
+        <div class="flex flex-col items-start text-start">
+          <slot />
+          <p
+            v-if="$slots.description"
+            :class="
+              cn(
+                'text-foreground/56 text-xs leading-4 font-normal',
+                props.descriptionColorClass ?? ''
+              )
+            "
+          >
+            <slot name="description" />
+          </p>
+        </div>
       </AccordionTrigger>
       <slot name="actions" />
     </div>
-
-    <p
-      v-if="$slots.description"
-      :class="
-        cn(
-          'mx-6 mt-2 align-middle text-xs leading-4 font-normal',
-          props.descriptionColorClass ? props.descriptionColorClass : ''
-        )
-      "
-    >
-      <slot name="description" />
-    </p>
   </AccordionHeader>
 </template>
